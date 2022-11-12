11757f06eaefd710c845f888bbcddcd52a56be02
yellowsink <yellowsink@users.noreply.github.com>
2022-11-12 00:57:33 +0000

    deploy: 941a21405f6034e86e4cdc97ae32930148e81463

--- quickreply/plugin.js
+++ quickreply/plugin.js
@@ -1,1 +1,144 @@
-(()=>{var d=Object.defineProperty;var p=Object.getOwnPropertyDescriptor;var b=Object.getOwnPropertyNames;var L=Object.prototype.hasOwnProperty;var R=(e,n)=>{for(var t in n)d(e,t,{get:n[t],enumerable:!0})},S=(e,n,t,s)=>{if(n&&typeof n=="object"||typeof n=="function")for(let l of b(n))!L.call(e,l)&&l!==t&&d(e,l,{get:()=>n[l],enumerable:!(s=p(n,l))||s.enumerable});return e};var C=e=>S(d({},"__esModule",{value:!0}),e);var x={};R(x,{onLoad:()=>M,onUnload:()=>Y});var{plugin:{store:o},flux:{dispatcher:i,stores:{ChannelStore:m,SelectedChannelStore:D,MessageStore:I}}}=shelter,{getChannel:w}=m,{getChannelId:c}=D,{getMessages:T}=I,A=()=>w(c()),u=new Set,r=-1,a=c(),E,y=Symbol("quickreply_deletePendingReply_int");function k(){if(!o.scroll)return;let e=document.querySelector('[data-list-id="chat-messages"]');Array.from(e.children).find(t=>t.firstChild?.className?.includes("replying-"))?.scrollIntoView({behavior:o.scrollSmooth?"smooth":void 0,block:"center"})}function G(e,n,t,s){typeof s>"u"&&(s=e.guild_id!==null),i.dispatch({type:"CREATE_PENDING_REPLY",channel:e,message:n,shouldMention:t&&!o.noPing,showMentionToggle:s}),setTimeout(k,100)}function f(e){i.dispatch({type:"DELETE_PENDING_REPLY",channelId:c(),...e})}function g(e){a!==e.channelId&&(a=e.channelId,r=-1)}function N(e){E=e.message.id}function _(e){E=void 0,e[y]||(r=-1)}async function P(e){if(!e.ctrlKey&&!e.metaKey||e.key!=="ArrowUp"&&e.key!=="ArrowDown")return;let n=(await T(c())).toArray().reverse(),t=n.findIndex(s=>s.id===E)||0;if(e.key==="ArrowUp"?r=t+1:e.key==="ArrowDown"&&(r=t-1),r>n.length&&(r=n.length),r<0)return f();f({[y]:!0}),G(A(),n[r],!u.has(c()))}function h({channelId:e,shouldMention:n}){n?u.delete(e):u.add(e)}function M(){o.scroll===void 0&&(o.scroll=!0),o.scrollSmooth===void 0&&(o.scrollSmooth=!0),o.noPing===void 0&&(o.noPing=!1),i.subscribe("CHANNEL_SELECT",g),i.subscribe("CREATE_PENDING_REPLY",N),i.subscribe("DELETE_PENDING_REPLY",_),i.subscribe("SET_PENDING_REPLY_SHOULD_MENTION",h),window.addEventListener("keydown",P)}function Y(){i.unsubscribe("CHANNEL_SELECT",g),i.unsubscribe("CREATE_PENDING_REPLY",N),i.unsubscribe("DELETE_PENDING_REPLY",_),i.unsubscribe("SET_PENDING_REPLY_SHOULD_MENTION",h),window.removeEventListener("keydown",P)}return C(x);})();
+ (() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+  // index.js
+  var quickreply_exports = {};
+  __export(quickreply_exports, {
+    onLoad: () => onLoad,
+    onUnload: () => onUnload
+  });
+  var {
+    plugin: { store },
+    flux: {
+      dispatcher,
+      stores: {
+        ChannelStore,
+        SelectedChannelStore,
+        MessageStore
+      }
+    }
+  } = shelter;
+  var { getChannel } = ChannelStore;
+  var { getChannelId } = SelectedChannelStore;
+  var { getMessages } = MessageStore;
+  var getCurrentChannel = () => getChannel(getChannelId());
+  var dontReplyStore = /* @__PURE__ */ new Set();
+  var messageIndex = -1;
+  var activeChannel = getChannelId();
+  var replyingToMessage = void 0;
+  var QRSymbol = Symbol("quickreply_deletePendingReply_int");
+  function scrollToReplyingMsg() {
+    if (!store.scroll)
+      return;
+    const messageContainer = document.querySelector(
+      '[data-list-id="chat-messages"]'
+    );
+    const replyingMsg = Array.from(messageContainer.children).find(
+      (elem) => elem.firstChild?.className?.includes("replying-")
+    );
+    replyingMsg?.scrollIntoView({
+      behavior: store.scrollSmooth ? "smooth" : void 0,
+      block: "center"
+    });
+  }
+  function createPendingReply(channel, message, shouldMention, showMentionToggle) {
+    if (typeof showMentionToggle === "undefined")
+      showMentionToggle = channel.guild_id !== null;
+    dispatcher.dispatch({
+      type: "CREATE_PENDING_REPLY",
+      channel,
+      message,
+      shouldMention: shouldMention && !store.noPing,
+      showMentionToggle
+    });
+    setTimeout(scrollToReplyingMsg, 100);
+  }
+  function deletePendingReply(data) {
+    dispatcher.dispatch({
+      type: "DELETE_PENDING_REPLY",
+      channelId: getChannelId(),
+      ...data
+    });
+  }
+  function channelSelect(data) {
+    if (activeChannel !== data.channelId) {
+      activeChannel = data.channelId;
+      messageIndex = -1;
+    }
+  }
+  function onCreatePendingReply(data) {
+    replyingToMessage = data.message.id;
+  }
+  function onDeletePendingReply(data) {
+    replyingToMessage = void 0;
+    if (!data[QRSymbol])
+      messageIndex = -1;
+  }
+  async function keyDown(event) {
+    if (!event.ctrlKey && !event.metaKey)
+      return;
+    if (event.key !== "ArrowUp" && event.key !== "ArrowDown")
+      return;
+    const messages = (await getMessages(getChannelId())).toArray().reverse();
+    const lastIndex = messages.findIndex((msg) => msg.id === replyingToMessage) || 0;
+    if (event.key === "ArrowUp")
+      messageIndex = lastIndex + 1;
+    else if (event.key === "ArrowDown")
+      messageIndex = lastIndex - 1;
+    if (messageIndex > messages.length)
+      messageIndex = messages.length;
+    if (messageIndex < 0)
+      return deletePendingReply();
+    deletePendingReply({
+      [QRSymbol]: true
+    });
+    createPendingReply(
+      getCurrentChannel(),
+      messages[messageIndex],
+      !dontReplyStore.has(getChannelId())
+    );
+  }
+  function onMentionChange({ channelId, shouldMention }) {
+    if (shouldMention)
+      dontReplyStore.delete(channelId);
+    else
+      dontReplyStore.add(channelId);
+  }
+  function onLoad() {
+    if (store.scroll === void 0)
+      store.scroll = true;
+    if (store.scrollSmooth === void 0)
+      store.scrollSmooth = true;
+    if (store.noPing === void 0)
+      store.noPing = false;
+    dispatcher.subscribe("CHANNEL_SELECT", channelSelect);
+    dispatcher.subscribe("CREATE_PENDING_REPLY", onCreatePendingReply);
+    dispatcher.subscribe("DELETE_PENDING_REPLY", onDeletePendingReply);
+    dispatcher.subscribe("SET_PENDING_REPLY_SHOULD_MENTION", onMentionChange);
+    window.addEventListener("keydown", keyDown);
+  }
+  function onUnload() {
+    dispatcher.unsubscribe("CHANNEL_SELECT", channelSelect);
+    dispatcher.unsubscribe("CREATE_PENDING_REPLY", onCreatePendingReply);
+    dispatcher.unsubscribe("DELETE_PENDING_REPLY", onDeletePendingReply);
+    dispatcher.unsubscribe("SET_PENDING_REPLY_SHOULD_MENTION", onMentionChange);
+    window.removeEventListener("keydown", keyDown);
+  }
+  return __toCommonJS(quickreply_exports);
+})();
