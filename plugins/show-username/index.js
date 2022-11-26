4c3b2a0926d3d6839c7c734d98d6026b32b12485
yellowsink <yellowsink@riseup.net>
2022-11-26 16:50:57 +0000

    add show-username

---
+++ plugins/show-username/index.js
@@ -0,0 +1,52 @@
+const {
+	flux: {
+		dispatcher,
+		stores: { GuildMemberStore, ChannelStore }
+	},
+	util: {
+		getFiber,
+		reactFiberWalker
+	},
+	observeDom
+} = shelter;
+
+function addUsernames() {
+	for (const e of document.querySelectorAll("[id^=message-username-]")) {
+		if (e?.dataset?.YSINK_SU) continue;
+		e.dataset.YSINK_SU = true;
+
+		const msg = reactFiberWalker(getFiber(e), "message", true).pendingProps?.message;
+		const authorUsername = msg.author?.username;
+		const authorId = msg?.author?.id;
+		const guildId = ChannelStore.getChannel(msg?.channel_id)?.guild_id;
+		const nick = GuildMemberStore.getNick(guildId, authorId);
+
+		if (!nick || !authorUsername) continue;
+
+		e.firstElementChild.textContent += ` (${authorUsername})`
+	}
+}
+
+// MESSAGE_CREATE: a new message is sent
+// CHANNEL_SELECT: the user switches servers
+// LOAD_MESSAGES_SUCCESS: new messages in viewport
+// UPDATE_CHANNEL_DIMENSIONS: the user scrolls back down perhaps
+const TRIGGERS = ["MESSAGE_CREATE", "CHANNEL_SELECT", "LOAD_MESSAGES_SUCCESS", "UPDATE_CHANNEL_DIMENSIONS"];
+
+function onDispatch() {
+	const unObserve = observeDom("[id^=message-username-]", () => {
+		unObserve();
+		queueMicrotask(addUsernames);
+	});
+
+	// maybe that message was created in another server? don't leave this forever.
+	setTimeout(unObserve, 500);
+}
+
+export function onLoad() {
+	for (const t of TRIGGERS) dispatcher.subscribe(t, onDispatch);
+}
+
+export function onUnload() {
+	for (const t of TRIGGERS) dispatcher.unsubscribe(t, onDispatch);
+}
