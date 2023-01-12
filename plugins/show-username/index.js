cbd0fff770c45371270717da5a78b64bed5e5098
yellowsink <yellowsink@riseup.net>
2023-01-12 21:10:21 +0000

    hugely bump SU perf Co-Authored-By: ioj4 <69911332+ioj4@users.noreply.github.com>

--- plugins/show-username/index.js
+++ plugins/show-username/index.js
@@ -1,53 +1,71 @@
 const {
-	flux: {
-		dispatcher,
-		stores: { GuildMemberStore, ChannelStore, RelationshipStore }
-	},
-	util: {
-		getFiber,
-		reactFiberWalker
-	},
-	observeDom
+  flux: {
+    dispatcher,
+    stores: {
+      GuildMemberStore,
+      ChannelStore,
+      SelectedChannelStore,
+      RelationshipStore,
+    },
+  },
+  util: { getFiber, reactFiberWalker },
+  observeDom,
 } = shelter;
 
-function addUsernames() {
-	for (const e of document.querySelectorAll("[id^=message-username-]")) {
-		if (e?.dataset?.YSINK_SU) continue;
-		e.dataset.YSINK_SU = true;
+function addUsername(e) {
+  if (e?.dataset?.ysink_su) return;
+  e.dataset.ysink_su = true;
 
-		const msg = reactFiberWalker(getFiber(e), "message", true).pendingProps?.message;
-		const authorUsername = msg.author?.username;
-		const authorId = msg?.author?.id;
-		const { type, guild_id: guildId } = ChannelStore.getChannel(msg?.channel_id);
-		// type = 0: Guild, 1: DM
-		const nick = type ? RelationshipStore.getNickname(authorId) : GuildMemberStore.getNick(guildId, authorId);
+  const msg = reactFiberWalker(getFiber(e), "message", true).pendingProps
+    ?.message;
 
-		if (!nick || !authorUsername) continue;
+  const authorUsername = msg.author?.username;
+  const authorId = msg?.author?.id;
 
-		e.firstElementChild.textContent += ` (${authorUsername})`
-	}
+  const { type, guild_id: guildId } = ChannelStore.getChannel(msg?.channel_id);
+
+  // type = 0: Guild, 1: DM
+  const nick = type
+    ? RelationshipStore.getNickname(authorId)
+    : GuildMemberStore.getNick(guildId, authorId);
+
+  if (!nick || !authorUsername) return;
+
+  e.firstElementChild.textContent += ` (${authorUsername})`;
 }
 
 // MESSAGE_CREATE: a new message is sent
 // CHANNEL_SELECT: the user switches servers
 // LOAD_MESSAGES_SUCCESS: new messages in viewport
 // UPDATE_CHANNEL_DIMENSIONS: the user scrolls back down perhaps
-const TRIGGERS = ["MESSAGE_CREATE", "CHANNEL_SELECT", "LOAD_MESSAGES_SUCCESS", "UPDATE_CHANNEL_DIMENSIONS"];
+const TRIGGERS = [
+  "MESSAGE_CREATE",
+  "CHANNEL_SELECT",
+  "LOAD_MESSAGES_SUCCESS",
+  "UPDATE_CHANNEL_DIMENSIONS",
+];
 
-function onDispatch() {
-	const unObserve = observeDom("[id^=message-username-]", () => {
-		unObserve();
-		queueMicrotask(addUsernames);
-	});
+function onDispatch(payload) {
+  // ignore MESSAGE_CREATEs from other channels
+  if (
+    payload.type === "MESSAGE_CREATE" &&
+    payload.channelId !== SelectedChannelStore.getChannelId()
+  )
+    return;
 
-	// maybe that message was created in another server? don't leave this forever.
-	setTimeout(unObserve, 500);
+  const unObserve = observeDom("[id^=message-username-]", (element) => {
+    unObserve();
+    addUsername(element);
+  });
+
+  // don't leave this forever, just in case!
+  setTimeout(unObserve, 500);
 }
 
 export function onLoad() {
-	for (const t of TRIGGERS) dispatcher.subscribe(t, onDispatch);
+  for (const t of TRIGGERS) dispatcher.subscribe(t, onDispatch);
 }
 
 export function onUnload() {
-	for (const t of TRIGGERS) dispatcher.unsubscribe(t, onDispatch);
+  for (const t of TRIGGERS) dispatcher.unsubscribe(t, onDispatch);
 }
