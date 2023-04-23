8efe3c2bdd468611c900d2827a5b111dc5bcfef6
yellowsink <yellowsink@riseup.net>
2023-04-23 14:57:46 +0100

    [pronoundb] create plugin

---
+++ plugins/pronoundb/db.ts
@@ -0,0 +1,83 @@
+const { UserProfileStore } = shelter.flux.stores;
+
+const pronouns = {
+	unspecified: "Unspecified",
+	hh: "he/him",
+	hi: "he/it",
+	hs: "he/she",
+	ht: "he/they",
+	ih: "it/him",
+	ii: "it/its",
+	is: "it/she",
+	it: "it/they",
+	shh: "she/he",
+	sh: "she/her",
+	si: "she/it",
+	st: "she/they",
+	th: "they/he",
+	ti: "they/it",
+	ts: "they/she",
+	tt: "they/them",
+	any: "Any pronouns",
+	other: "Other pronouns",
+	ask: "Ask me my pronouns",
+	avoid: "Avoid pronouns, use my name",
+};
+
+const pronounsToSearch = Object.values(pronouns).filter((p) => p.includes("/"));
+
+export const fromStore = (id) => {
+	const profile = UserProfileStore.getUserProfile(id);
+	if (typeof profile?.bio !== "string") return;
+	return pronounsToSearch.find((p) => profile.bio.includes(p));
+};
+
+const endpoint =
+	"https://pronoundb.org/api/v1/lookup-bulk?platform=discord&ids=";
+
+let batch = new Map<string, (v: string) => void>();
+let currentlyQueued = false;
+const cache = new Map<string, string>();
+
+const BATCH_TIME = 150;
+
+const fetchBatch = async () => {
+	const currentBatch = batch;
+	batch = new Map();
+
+	const ids = [...currentBatch.keys()].join();
+
+	try {
+		const res = await fetch(endpoint + ids).then((r) => r.json());
+
+		for (const uid in res)
+			if (currentBatch.has(uid) && res[uid] && res[uid] !== "unspecified") {
+				const prettyPronouns = pronouns[res[uid]];
+				currentBatch.get(uid)?.(prettyPronouns);
+				cache.set(uid, prettyPronouns);
+			}
+
+		// make sure all promises resolve
+		for (const [uid, func] of currentBatch.entries()) {
+			func(undefined);
+			if (!cache.has(uid)) cache.set(uid, undefined);
+		}
+	} finally {
+		currentlyQueued = false;
+	}
+};
+
+export const fetchPronouns = (uid) =>
+	cache.has(uid)
+		? Promise.resolve(cache.get(uid))
+		: new Promise<string>((res) => {
+				// dont leave hanging
+				setTimeout(res, 3000);
+
+				batch.set(uid, res);
+
+				if (!currentlyQueued) {
+					currentlyQueued = true;
+					setTimeout(fetchBatch, BATCH_TIME);
+				}
+		  });
---
+++ plugins/pronoundb/index.tsx
@@ -0,0 +1,57 @@
+const {
+	flux: { dispatcher },
+	observeDom,
+	util: { getFiber, reactFiberWalker },
+	ui: { Space },
+} = shelter;
+
+import { fetchPronouns, fromStore } from "./db.js";
+
+// I love async!
+const patchedEls = new WeakSet<Element>();
+
+async function inject(el) {
+	//if (el.parentElement.querySelector('[data-ys-prdb]')) return;
+	if (patchedEls.has(el)) return;
+	patchedEls.add(el);
+
+	const authorId = reactFiberWalker(getFiber(el), "message", true)?.pendingProps
+		?.message?.author?.id;
+	if (!authorId) return;
+
+	const pronouns = (await fetchPronouns(authorId)) ?? fromStore(authorId);
+
+	if (!pronouns) return;
+
+	el.insertAdjacentElement(
+		"beforebegin",
+		(
+			<span>
+				<Space />
+				{pronouns} •
+				<Space />
+			</span>
+		) as HTMLSpanElement,
+	);
+}
+
+function onDispatch() {
+	const unObserve = observeDom("h3 time[id^=message-timestamp]", (el) => {
+		unObserve();
+		inject(el);
+	});
+
+	setTimeout(unObserve, 500);
+}
+
+const TRIGGERS = [
+	"MESSAGE_CREATE",
+	"CHANNEL_SELECT",
+	"UPDATE_CHANNEL_DIMENSIONS",
+	"MESSAGE_UPDATE",
+];
+
+TRIGGERS.forEach((t) => dispatcher.subscribe(t, onDispatch));
+
+export const onUnload = () =>
+	TRIGGERS.forEach((t) => dispatcher.unsubscribe(t, onDispatch));
