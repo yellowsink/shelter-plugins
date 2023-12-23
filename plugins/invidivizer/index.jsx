6deebc2e6c391beed6000564a000def269b9cadf
yellowsink <yellowsink@riseup.net>
2023-12-23 20:47:47 +0000

    [invidivizer] add (req by link)

---
+++ plugins/invidivizer/index.jsx
@@ -0,0 +1,75 @@
+const {
+	plugin: { store },
+	observeDom,
+	flux: { dispatcher, storesFlat: { SelectedChannelStore } },
+	util: { reactFiberWalker, getFiber },
+	ui: { Header, HeaderTags, TextBox },
+} = shelter;
+
+store.instance ??= "invidious.slipfox.xyz";
+
+// taken and improved from more-embeds
+
+const TRIGGERS = [
+	"MESSAGE_CREATE",
+	"MESSAGE_UPDATE",
+	"UPDATE_CHANNEL_DIMENSIONS",
+];
+
+function handleDispatch(payload) {
+	if (!store.instance) return;
+	if (
+		(payload.type === "MESSAGE_CREATE" || payload.type === "MESSAGE_UPDATE") &&
+		payload.message.channel_id !== SelectedChannelStore.getChannelId()
+	)
+		return;
+
+	const unobs = observeDom(
+		`[id^="chat-messages-"] article:not([data-invidivizer])`,
+		(e) => {
+			// mutex
+			e.dataset.invidivizer = "1";
+			unobs();
+
+			const found = reactFiberWalker(getFiber(e), "embed", true)?.memoizedProps
+				?.embed?.url;
+			if (
+				!(typeof found === "string") ||
+				!found.startsWith("https://www.youtube.com")
+			)
+				return;
+
+			const match = found.match(/v=([a-zA-Z0-9-_]+)/);
+			if (!match?.[1]) return;
+
+			e.style.display = "none";
+			e.insertAdjacentElement(
+				"afterend",
+				<iframe
+					style="border: 0; width: 100%; max-width: 600px; aspect-ratio: 16/9"
+					src={`https://${store.instance}/embed/${match[1]}`}
+					allow="fullscreen"
+				/>,
+			);
+		},
+	);
+
+	setTimeout(unobs, 1000); // dangling
+}
+
+for (const t of TRIGGERS) dispatcher.subscribe(t, handleDispatch);
+
+export function onUnload() {
+	for (const t of TRIGGERS) dispatcher.unsubscribe(t, handleDispatch);
+}
+
+export const settings = () => (
+	<>
+		<Header tag={HeaderTags.H3}>Invidious Instance</Header>
+		<TextBox
+			placeholder="my.instance.com"
+			value={store.instance}
+			onInput={(v) => (store.instance = v)}
+		/>
+	</>
+);
