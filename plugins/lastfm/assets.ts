64db941132d6ed79d94436b1e8b4d926175f502b
Hazel Atkinson <yellowsink@riseup.net>
2024-04-25 12:38:08 +0100

    [lastfm] remove use of webpack

--- plugins/lastfm/assets.ts
+++ plugins/lastfm/assets.ts
@@ -1,5 +1,54 @@
-import { webpackChunk, createApi } from "@cumjar/websmack";
 import { DISCORD_APP_ID } from "./cfg";
+
+/*
+example exchange made by the real fetchAssetIds function:
+
+###
+POST https://discord.com/api/v9/applications/1107251687984472144/external-assets HTTP/2
+> Content-Type: application/json
+> headers: go here
+
+{"urls":["https://coverartarchive.org/release/d131c1ab-ffe1-4f68-941d-c1c238a91f50/front"]}
+
+###
+HTTP/2 200
+> headers: go here
+
+[
+    {
+        "url": "https://coverartarchive.org/release/d131c1ab-ffe1-4f68-941d-c1c238a91f50/front",
+        "external_asset_path": "external/BXbyYaCdwy2DMWxTRmsvqVHDvXgf6LQmzgTwVRIBass/https/coverartarchive.org/release/d131c1ab-ffe1-4f68-941d-c1c238a91f50/front"
+    }
+]
+*/
+
+const { post } = shelter.http;
+
+const cache = new Map<string, string | undefined>();
+
+export async function getAsset(url: string) {
+	if (cache.has(url)) {
+		return cache.get(url);
+	}
+
+	const res = await post({
+		url: `/applications/${DISCORD_APP_ID}/external-assets`,
+		body: JSON.stringify({ urls: [url] }),
+		oldFormErrors: false,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	if (res.ok) {
+		const path = JSON.parse(res.body)[0].external_asset_path;
+		cache.set(url, path);
+		return path;
+	}
+	cache.set(url, undefined);
+}
+
+/*import { webpackChunk, createApi } from "@cumjar/websmack";
 
 const modules = webpackChunk();
 const api = modules && createApi([undefined, ...modules]);
@@ -15,3 +64,4 @@ export const getAsset = (url: string): Promise<string> =>
 	!mod.fetchAssetIds
 		? undefined
 		: mod.fetchAssetIds(DISCORD_APP_ID, [url, undefined]).then((v) => v[0]);
+*/
