f6798e53dee873960f3be7e80668b1eddeb65fd5
yellowsink <yellowsink@users.noreply.github.com>
2025-01-12 12:57:36 +0000

    deploy: dbb0c6bd28c68c16ea16feee911cff63c5828fbc

--- antitrack/plugin.js
+++ antitrack/plugin.js
@@ -3,12 +3,38 @@
 "use strict";
 
 //#region plugins/antitrack/index.js
-const { scoped } = shelter.plugin;
+const { plugin: { scoped }, util: { log } } = shelter;
 try {
-	window.__SENTRY__.hub.getClient().getOptions().enabled = false;
-	Object.keys(console).forEach((x) => console[x] = console[x].__sentry_original__ ?? console[x]);
-} catch {}
-scoped.http.intercept("POST", /^\/science|^\/error-reporting-proxy/, () => {});
+	if (DiscordSentry) DiscordSentry.close();
+else Object.defineProperty(window, "DiscordSentry", {
+		set(v) {
+			delete window.DiscordSentry;
+			window.DiscordSentry = v;
+			setTimeout(v.close);
+		},
+		enumerable: false,
+		configurable: true
+	});
+} catch (e) {
+	log(`[Antitrack] Error while disabling Sentry ${e}`);
+}
+try {
+	Object.keys(console).forEach((func) => {
+		const original = console[func]?.__sentry_original__;
+		if (!original) return;
+		console[func] = original;
+	});
+} catch (e) {
+	log(`[Antitrack] Error while unpatching console functions ${e}`);
+}
+scoped.http.intercept("POST", /^\/science/, () => {});
+const metricsRe = /^https?:\/\/([a-z0-9\-]+\.)?discord\.com\/api\/v\d+\/metrics/;
+const getSentryProp = (obj) => obj[Object.keys(obj).find((k) => k.startsWith("__sentry_xhr"))];
+scoped.patcher.instead("send", XMLHttpRequest.prototype, function(args, orig) {
+	const url = getSentryProp(this)?.url;
+	if (typeof url === "string" && metricsRe.test(url)) return;
+	return orig.apply(this, args);
+});
 
 //#endregion
 })();
